0b79a7b2e3d85583706640aadab0a850de13c4b5
RavgoYT <88573982+RavgoYT@users.noreply.github.com>
2023-04-09 18:07:11 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div className="w-full flex flex-col items-start border-t-2 border-slate-800 px-4 py-8 dark:border-opacity-50 mb-20">
             <h1 className="text-black dark:text-white/50 text-2xl font-semibold">The Akira Project</h1>
-            <h2 className="text-black/60 dark:text-white/30 text-base"><3 • 2023</h2>
+            <h2 className="text-black/60 dark:text-white/30 text-base">Rav • 2023</h2>
         </div>
     );
 };
